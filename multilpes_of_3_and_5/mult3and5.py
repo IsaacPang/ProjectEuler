47a9dc14496a99ae9fcefa10610daa847300fd04
Isaac Pang <isaac.pang91@gmail.com>
2020-05-13 16:10:10 +1000

    updated recursion limit for mult3&5 question

--- multilpes_of_3_and_5/mult3and5.py
+++ multilpes_of_3_and_5/mult3and5.py
@@ -1,5 +1,11 @@
+import sys
+
+sys.setrecursionlimit(2000)
+
+
 def m35(x):
     x -= 1
+
     def count35(num, total=0):
         if num == 0:
             return total
@@ -7,7 +13,9 @@ def m35(x):
             return count35(num-1, total + num)
         else:
             return count35(num-1, total)
+
     return count35(x)
+
 
 print(m35(1000))
 
